7b7bf578d0e69041bbeeb1cf82106fe08010ecf0
Alvaro Torres Cogollo <atorrescogollo@gmail.com>
2019-05-15 14:01:58 +0200

    initCA.sh: CA key and certificate self-signed

---
+++ initCA.sh
@@ -0,0 +1,83 @@
+#!/usr/local/bin/bash
+
+cd `dirname $0`
+WORKING_DIR="`pwd`/CA"
+CONF_DIR="$WORKING_DIR/configs"
+CERTS_DIR="$WORKING_DIR/certs"
+SIGNED_CERTS_DIR="$WORKING_DIR/signed_certs"
+PRIVATE_DIR="$WORKING_DIR/private"
+
+# Formating paths
+fnfcount=0
+WORKING_DIR=`realpath $WORKING_DIR`
+(( fnfcount+=`echo $?` ))
+CONF_DIR=`realpath $CONF_DIR`
+(( fnfcount+=`echo $?` ))
+SIGNED_CERTS_DIR=`realpath $SIGNED_CERTS_DIR`
+(( fnfcount+=`echo $?` ))
+CERTS_DIR=`realpath $CERTS_DIR`
+(( fnfcount+=`echo $?` ))
+PRIVATE_DIR=`realpath $PRIVATE_DIR`
+(( fnfcount+=`echo $?` ))
+
+# Colors
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [ $fnfcount -gt 0 ]; then
+	echo -e "${RED}[ERROR]:${NC} $fnfcount directories not found. Please, review initial variables." >&2
+	exit 1
+fi
+
+
+echo -e "${BLUE}[INFO]:${NC} Getting cnf files..."
+i=0
+while read file
+do
+	conffiles[ $i ]=$file
+	(( i++ ))
+done < <(ls $CONF_DIR/*.cnf 2> /dev/null)
+
+if [ $i -eq 0 ]; then
+	echo -e "${RED}[ERROR]:${NC} There are no config files availables in $CONF_DIR."
+	echo -e "${RED}[ERROR]:${NC} Please, generate *.cnf files." >&2
+	exit 2
+fi
+
+echo "Available cnf files:"
+echo "===================="
+i=0
+for file in ${conffiles[@]}; do
+	echo "$i.-   $file"
+	(( i++ ))
+done
+
+read -p "     -> Select your cnf file index: " index
+echo "===================="
+if ! [[ "$index" =~ ^[0-9]+$ ]] || (( $index >= $i )); then
+	echo -e "${RED}[ERROR]:${NC} Wrong selection!"
+	exit 3
+fi
+
+CONF_FILE=${conffiles[$index]}
+echo -e "${BLUE}[INFO]:${NC} Selected $CONF_FILE."
+
+
+cmd="openssl req -x509 -newkey rsa:2048 -out $CERTS_DIR/cacert.crt -outform PEM -days 3560"
+echo -e "${BLUE}[INFO]:${NC} Command: '$cmd'"
+
+echo -ne "${BLUE}[INFO]:${NC} Generate private key and CA certificate? "
+read -rp "[Y/n] " ok
+if [[ "$ok" =~ ^(([yY])+|([\ ]*))$ ]]; then
+	echo -e "${BLUE}[INFO]:${NC} Setting openssl config file..."
+	export OPENSSL_CONF=$CONF_FILE
+	printenv OPENSSL_CONF
+	echo -e "${BLUE}[INFO]:${NC} Initializating $WORKING_DIR/serial and $WORKING_DIR/index.txt"
+	echo '01' > $WORKING_DIR/serial
+	cp /dev/null $WORKING_DIR/index.txt
+	`$cmd`	
+else
+	echo -e "${RED}[ERROR]:${NC} Operation aborted"
+fi
+
